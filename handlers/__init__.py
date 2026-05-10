1f484bd53dccbfcb935dbac30ed39643638ec0da
miestaglobal-hue <miestaglobal@gmail.com>
2026-05-10 22:18:37 +0300

    Initialize handlers module with routers

---
+++ handlers/__init__.py
@@ -0,0 +1,6 @@
+from .menu import router as menu_router
+from .register import router as register_router
+from .swipe import router as swipe_router
+from .invite import router as invite_router
+
+__all__ = ["menu_router", "register_router", "swipe_router", "invite_router"]
